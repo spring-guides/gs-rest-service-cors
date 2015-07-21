fcf17441ce0754eaf4dad301f05d9289c9263133
Sebastien Deleuze <sdeleuze@pivotal.io>
2015-07-21 14:23:23 +0200

    Upgrade to use Spring Framework 4.2 CORS support
    This commit introduces the following changes:
     - Spring Boot CLI is not needed anymore
     - CORS configuration through @CrossOrigin annotated controller
     - CORS configuration through JavaConfig
    
    Resolves #6

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -2,6 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +13,17 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    // tag::corsConfigurer[]
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
+            }
+        };
+    }
+    // end::corsConfigurer[]
+
 }
--- test/run.sh
+++ test/run.sh
@@ -1,16 +1,17 @@
 cd $(dirname $0)
 cd ../complete
 mvn clean package
-java -jar target/gs-rest-service-cors-0.1.0.jar &
+java -jar target/gs-rest-service-cors-0.2.0.jar &
 PID=$!
 sleep 15
 curl -s http://localhost:8080/greeting > target/actual.json
+curl -s http://localhost:8080/greeting-javaconfig > target/actual-javaconfig.json
 kill -9 $PID
 
 echo "Let's look at the actual results: `cat target/actual.json`"
-echo "And compare it to: `cat ../test/expected.json`"
+echo "And compare it to: `cat ../test/expected-first.json`"
 
-if diff -w ../test/expected.json target/actual.json
+if diff -w ../test/expected-first.json target/actual.json
     then
         echo SUCCESS
         let ret=0
@@ -19,19 +20,34 @@ if diff -w ../test/expected.json target/actual.json
         let ret=255
         exit $ret
 fi
+
+echo "Let's look at the actual results: `cat target/actual-javaconfig.json`"
+echo "And compare it to: `cat ../test/expected-second.json`"
+
+if diff -w ../test/expected-second.json target/actual-javaconfig.json
+    then
+        echo SUCCESS
+        let ret=0
+    else
+        echo FAIL
+        let ret=255
+        exit $ret
+fi
+
 rm -rf target
 
-./gradlew build
-java -jar build/libs/gs-rest-service-cors-0.1.0.jar &
+./gradlew clean build
+java -jar build/libs/gs-rest-service-cors-0.2.0.jar &
 PID=$!
 sleep 15
 curl -s http://localhost:8080/greeting > build/actual.json
+curl -s http://localhost:8080/greeting-javaconfig > build/actual-javaconfig.json
 kill -9 $PID
 
 echo "Let's look at the actual results: `cat build/actual.json`"
-echo "And compare it to: `cat ../test/expected.json`"
+echo "And compare it to: `cat ../test/expected-first.json`"
 
-if diff -w ../test/expected.json build/actual.json
+if diff -w ../test/expected-first.json build/actual.json
     then
         echo SUCCESS
         let ret=0
@@ -40,6 +56,20 @@ if diff -w ../test/expected.json build/actual.json
         let ret=255
         exit $ret
 fi
+
+echo "Let's look at the actual results: `cat build/actual-javaconfig.json`"
+echo "And compare it to: `cat ../test/expected-second.json`"
+
+if diff -w ../test/expected-second.json build/actual-javaconfig.json
+    then
+        echo SUCCESS
+        let ret=0
+    else
+        echo FAIL
+        let ret=255
+        exit $ret
+fi
+
 rm -rf build
 
 cd ../initial
@@ -51,7 +81,7 @@ exit $ret
 fi
 rm -rf target
 
-./gradlew compileJava
+./gradlew clean compileJava
 ret=$?
 if [ $ret -ne 0 ]; then
 exit $ret

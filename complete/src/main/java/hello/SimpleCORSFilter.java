c676ba14001d5af1e89a770322c287d62a71afa4
Ann Addicks <Ann.Addicks@gmail.com>
2015-09-19 14:49:36 -0400

    added PUT method and extra headers
    I ran into an issue with firefox and ng-admin sending Content-Type and it was blocked.

--- complete/src/main/java/hello/SimpleCORSFilter.java
+++ complete/src/main/java/hello/SimpleCORSFilter.java
@@ -15,9 +15,9 @@ public class SimpleCORSFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		chain.doFilter(req, res);
 	}
 

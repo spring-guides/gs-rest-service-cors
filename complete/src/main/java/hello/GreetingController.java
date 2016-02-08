377ce0c1e6a4001f85fc9a57fc0e14fa5e5f027e
Dave Syer <dsyer@pivotal.io>
2016-02-08 09:43:36 +0000

    Tweak for production deployment

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -3,28 +3,29 @@ package hello;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
-    @CrossOrigin(origins = "http://localhost:9000")
-    @RequestMapping("/greeting")
-    public @ResponseBody Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
-        System.out.println("==== in greeting ====");
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
+	@CrossOrigin
+	@RequestMapping("/greeting")
+	public @ResponseBody Greeting greeting(
+			@RequestParam(required = false, defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 
-    @RequestMapping("/greeting-javaconfig")
-    public @ResponseBody Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
-        System.out.println("==== in greeting ====");
-        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-    }
+	@RequestMapping("/greeting-javaconfig")
+	public @ResponseBody Greeting greetingWithJavaconfig(
+			@RequestParam(required = false, defaultValue = "World") String name) {
+		System.out.println("==== in greeting ====");
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 
 }

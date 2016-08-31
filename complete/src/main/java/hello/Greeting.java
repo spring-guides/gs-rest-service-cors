a1319b70590bd077a1273da4e0155a6277725ac8
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-08-31 11:29:35 +0200

    Polish the code and add integration tests

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -5,6 +5,11 @@ public class Greeting {
     private final long id;
     private final String content;
 
+    public Greeting() {
+        this.id = -1;
+        this.content = "";
+    }
+
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -2,27 +2,26 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @CrossOrigin(origins = "http://localhost:9000")
-    @RequestMapping("/greeting")
-    public @ResponseBody Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
         System.out.println("==== in greeting ====");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
-    @RequestMapping("/greeting-javaconfig")
-    public @ResponseBody Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
+    @GetMapping("/greeting-javaconfig")
+    public Greeting greetingWithJavaconfig(@RequestParam(required=false, defaultValue="World") String name) {
         System.out.println("==== in greeting ====");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }

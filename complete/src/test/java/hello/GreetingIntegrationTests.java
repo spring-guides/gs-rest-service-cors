b7d94e1f7a752c5a95f5536db68ad78191ceeea8
Dave Syer <dsyer@pivotal.io>
2017-01-26 10:19:03 +0000

    Tidy up test a bit (using RequestEntity)

--- complete/src/test/java/hello/GreetingIntegrationTests.java
+++ complete/src/test/java/hello/GreetingIntegrationTests.java
@@ -1,17 +1,17 @@
 package hello;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
+import java.net.URI;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,31 +19,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GreetingIntegrationTests {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+  @Autowired
+  private TestRestTemplate restTemplate;
 
-    @Test
-    public void corsWithAnnotation() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.ORIGIN, "http://localhost:9000");
-        HttpEntity<?> requestEntity = new HttpEntity(headers);
-        ResponseEntity<Greeting> entity = this.restTemplate.exchange("/greeting", HttpMethod.GET, requestEntity, Greeting.class);
-        assertEquals(HttpStatus.OK, entity.getStatusCode());
-        assertEquals("http://localhost:9000", entity.getHeaders().getAccessControlAllowOrigin());
-        Greeting greeting = entity.getBody();
-        assertEquals("Hello, World!", greeting.getContent());
-    }
+  @Test
+  public void corsWithAnnotation() throws Exception {
+    ResponseEntity<Greeting> entity = this.restTemplate.exchange(
+        RequestEntity.get(uri("/greeting")).header(HttpHeaders.ORIGIN, "http://localhost:9000").build(),
+        Greeting.class);
+    assertEquals(HttpStatus.OK, entity.getStatusCode());
+    assertEquals("http://localhost:9000", entity.getHeaders().getAccessControlAllowOrigin());
+    Greeting greeting = entity.getBody();
+    assertEquals("Hello, World!", greeting.getContent());
+  }
 
-    @Test
-    public void corsWithJavaconfig() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.ORIGIN, "http://localhost:9000");
-        HttpEntity<?> requestEntity = new HttpEntity(headers);
-        ResponseEntity<Greeting> entity = this.restTemplate.exchange("/greeting-javaconfig", HttpMethod.GET, requestEntity, Greeting.class);
-        assertEquals(HttpStatus.OK, entity.getStatusCode());
-        assertEquals("http://localhost:9000", entity.getHeaders().getAccessControlAllowOrigin());
-        Greeting greeting = entity.getBody();
-        assertEquals("Hello, World!", greeting.getContent());
-    }
+  @Test
+  public void corsWithJavaconfig() {
+    ResponseEntity<Greeting> entity = this.restTemplate.exchange(
+        RequestEntity.get(uri("/greeting-javaconfig")).header(HttpHeaders.ORIGIN, "http://localhost:9000").build(),
+        Greeting.class);
+    assertEquals(HttpStatus.OK, entity.getStatusCode());
+    assertEquals("http://localhost:9000", entity.getHeaders().getAccessControlAllowOrigin());
+    Greeting greeting = entity.getBody();
+    assertEquals("Hello, World!", greeting.getContent());
+  }
+
+  private URI uri(String path) {
+    return restTemplate.getRestTemplate().getUriTemplateHandler().expand(path);
+  }
 
 }

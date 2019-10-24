11362d0da0b7b2db01cd0140958c14f73d2e0394
Jay Bryant <jbryant@pivotal.io>
2019-10-24 13:37:23 -0500

    Upgraded to Spring Boot 2.2
    I upgraded the build files to use Spring Boot 2.2.0, which necessitated changing the tests to use JUnit 5, replacing the Gradle wrappers, and modifying the test script (because Spring Boot 2.2 generates different file names than the previous version).

--- complete/src/test/java/com/example/restservicecors/GreetingIntegrationTests.java
+++ complete/src/test/java/com/example/restservicecors/GreetingIntegrationTests.java
@@ -1,11 +1,11 @@
 package com.example.restservicecors;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.net.URI;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -13,9 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GreetingIntegrationTests {
 
